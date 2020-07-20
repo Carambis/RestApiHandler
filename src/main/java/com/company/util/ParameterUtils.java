48ac6ac9b9c8cfe3f715e396867515529bb79bda
Carambis <lepeshko1997@mail.ru>
2020-07-20 18:54:00 +0300

    add string and parameter utils

---
+++ src/main/java/com/company/util/ParameterUtils.java
@@ -0,0 +1,8 @@
+package com.company.util;
+
+public class ParameterUtils {
+
+	public static final String REST_API_PARAMETER_NAME = "restApiParameterName";
+	public static final String BUCKET_NAME = "s3BucketName";
+	public static final String ID = "id";
+}
---
+++ src/main/java/com/company/util/StringUtils.java
@@ -0,0 +1,7 @@
+package com.company.util;
+
+public class StringUtils {
+
+	public final static String RIGHT_SLASH = "/";
+	public final static String UNDERSCORE = "_";
+}
